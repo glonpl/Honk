4ac582e9fa45998e287c4f03a19dde5b572a0079
Bartosz Ratajczyk <bratajczyk.software+github@gmail.com>
2023-06-08 14:57:50 +0200

    Update bad.bat

--- bad.bat
+++ bad.bat
@@ -1,5 +1,5 @@
 @echo off
-set /p counter=<%APPDATA%\Honk-master\counter.txt
-FOR /L %%G IN (1,1,%counter%) DO start %APPDATA%\Honk-master\GooseDesktop.exe
+set /p counter=<%APPDATA%\Honk-main\counter.txt
+FOR /L %%G IN (1,1,%counter%) DO start %APPDATA%\Honk-main\GooseDesktop.exe
 set /A counter=%counter% + 1
-echo %counter% >%APPDATA%\Honk-master\counter.txt
+echo %counter% >%APPDATA%\Honk-main\counter.txt
